527ef5146d2dd8504f2bdc0aaa25ede0b52c1c77
Jannis241 <jannis2401p@gmail.com>
2024-06-25 12:43:20 +0200

    automatic

--- src/App.java
+++ src/App.java
@@ -239,10 +239,6 @@ public class App {
     }
 
     public static void main(String[] args) throws Exception {
-        System.out.println("funktioniert das echt?");
-        System.out.println("funktioniert das echt?");
-        System.out.println("funktioniert das echt?");
-        System.out.println("funktioniert das echt?");
         System.out.println("--------- <DedeType> ---------");
         System.out.println("<Start> Starting window..");
 
